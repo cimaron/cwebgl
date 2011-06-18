f15d9c49212773900b3fd88a9b902a4fce09b93e
cshanahan.cimaron@gmail.com <cshanahan.cimaron@gmail.com@ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb>
2011-06-18 00:07:42 +0000

    Importing WebGL API
    git-svn-id: https://cwebgl.googlecode.com/svn/trunk@4 ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb

---
+++ WebGL/WebGLShader.js
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2011 Cimaron Shanahan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+//----------------------------------------------------------------------------------------
+//	class WebGLShader
+//----------------------------------------------------------------------------------------
+
+function cWebGLShader(context, /*GLenum*/ type) {	
+
+	//members
+	this._type = null;
+	this._source = null;
+	
+	//Call constructor
+	this.construct(context, type);
+}
+
+//----------------------------------------------------------------------------------------
+//	Class Magic
+//----------------------------------------------------------------------------------------
+
+__WebGLShader = new cWebGLObject;
+cWebGLShader.prototype = __WebGLShader;
+
+//----------------------------------------------------------------------------------------
+//	Methods
+//----------------------------------------------------------------------------------------
+
+__WebGLShader.cWebGLShader = function(context, type) {
+	this.cWebGLObject(context);
+	this._type = type;
+	this._source = '';
+	this.setObject(this.context().graphicsContext3D().createShader(type));
+}
+
+//public:
+
+__WebGLShader.getType = function() {
+	return this._type;
+}
+
+__WebGLShader.getSource = function() {
+	return this._source;	
+}
+
+__WebGLShader.setSource = function(/*DOMString*/ source) {
+	this._source = source;
+}
+
+//private:
+
