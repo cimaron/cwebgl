090b63c559dc6d56ec78dbc5940c9e8c1d215695
cshanahan.cimaron@gmail.com <cshanahan.cimaron@gmail.com@ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb>
2011-06-23 23:18:33 +0000

    Cleanup/changes to compiler, linker and test programs
    git-svn-id: https://cwebgl.googlecode.com/svn/trunk@13 ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb

--- cnvGL/includes/shaderCompiler/compiler.js
+++ cnvGL/includes/shaderCompiler/compiler.js
@@ -1,52 +1,94 @@
+/*
+Copyright (c) 2011 Cimaron Shanahan
 
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+//----------------------------------------------------------------------------------------
+//	class ShaderCompiler
+//----------------------------------------------------------------------------------------
 
 function ShaderCompiler(type) {
+
+	//member variables:
 	this.type = null;
+
+	//call constructor
 	this.construct(type);
 }
 
-function __ShaderCompiler() {
-	this.ShaderCompiler = function(type) {
-		this.type = type;
-	}
+//----------------------------------------------------------------------------------------
+//	Class Magic
+//----------------------------------------------------------------------------------------
 
-	this.compile = function(shader_source, shader_obj) {
-		if (this.type == GL_VERTEX_SHADER) {
-			this.result = defaultVertex(shader_obj);
-		} else {
-			this.result = defaultFragment(shader_obj);
-		}
-		return true;
-	}
+__ShaderCompiler = new pClass;
+ShaderCompiler.prototype = __ShaderCompiler;
 
-	this._compile = function() {
-		//preprocess
-		var pp = new ShaderCompilerPreprocessor();
-		processed_source = pp.preprocess(shader_source);
-		if (!processed_source) {
-			//get some error from preprocessor	
-		}
-	
-		var lexer = new __glCompileShaderLexer();
-		var parser = new __glCompileShaderParser();
-		var parsed = parser.parse(lexer, processed_source);
+//----------------------------------------------------------------------------------------
+//	Methods
+//----------------------------------------------------------------------------------------
 
-		if (!parsed) {
-			//get some error from 
-		}
-
-		var symbol_table = parsed.symbol_table;
-		var parse_tree = parsed.parse_tree;
-
-		var generator = new __glCompileShaderGenerator(symbol_table, parse_tree);
-	}
+__ShaderCompiler.ShaderCompiler = function(type) {
+	this.type = type;
 }
-__ShaderCompiler.prototype = new pClass;
-ShaderCompiler.prototype = new __ShaderCompiler;
 
+//public:
 
+__ShaderCompiler.compile = function(shader_source, shader_obj) {
+	if (this.type == GL_VERTEX_SHADER) {
+		this.result = defaultVertex(shader_obj);
+	} else {
+		this.result = defaultFragment(shader_obj);
+	}
+	return true;
+}
 
+/*
 
+__ShaderCompiler._compile = function() {
+	//preprocess
+	var pp = new ShaderCompilerPreprocessor();
+	processed_source = pp.preprocess(shader_source);
+	if (!processed_source) {
+		//get some error from preprocessor	
+	}
+
+	var lexer = new __glCompileShaderLexer();
+	var parser = new __glCompileShaderParser();
+	var parsed = parser.parse(lexer, processed_source);
+
+	if (!parsed) {
+		//get some error from 
+	}
+
+	var symbol_table = parsed.symbol_table;
+	var parse_tree = parsed.parse_tree;
+
+	var generator = new __glCompileShaderGenerator(symbol_table, parse_tree);
+}
+
+*/
+
+//----------------------------------------------------------------------------------------
+//	The following are preconstructed sample programs based on the specific test program
+//	They will be removed and replaced when the actual compiler is built
+//----------------------------------------------------------------------------------------
 
 /*
 #ifdef GL_ES
@@ -63,8 +105,8 @@ function defaultFragment(shader_obj) {
 
 	//actual program
 	var program = 
-	"function() {"+
-	"	gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);"+
+	"function(__data) {"+
+	"	__data.gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);"+
 	"}";
 	shader_obj.object_code = program;
 	shader_obj.compile_status = true;
@@ -95,8 +137,8 @@ function defaultVertex(shader_obj) {
 
 	//program
 	var program = 
-	"function() {"+
-	"	gl_Position = mult4x4(mult4x4(uPMatrix, uMVMatrix), vec4(aVertexPosition, 1.0));"+
+	"function(__data) {"+
+	"	__data.gl_Position = mult4x4(mult4x4(__data.uPMatrix, __data.uMVMatrix), vec4(__data.aVertexPosition, 1.0));"+
 	"}";
 
 	shader_obj.object_code = program;
--- cnvGL/includes/shaderLinker/linker.js
+++ cnvGL/includes/shaderLinker/linker.js
@@ -1,107 +1,147 @@
+/*
+Copyright (c) 2011 Cimaron Shanahan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+//----------------------------------------------------------------------------------------
+//	class ShaderLinker
+//----------------------------------------------------------------------------------------
 
 function ShaderLinker(program_obj) {
 
+	//member variables:
 	this.program_obj = null;
 
+	//Call constructor
 	this.construct(program_obj);
 }
 
-function __ShaderLinker() {
-	
-	this.ShaderLinker = function(program_obj) {
-		this.program_obj = program_obj;
-		this.initialize(program_obj);
+//----------------------------------------------------------------------------------------
+//	Class Magic
+//----------------------------------------------------------------------------------------
+
+__ShaderLinker = new pClass;
+ShaderLinker.prototype = __ShaderLinker;
+
+//----------------------------------------------------------------------------------------
+//	Methods
+//----------------------------------------------------------------------------------------
+
+__ShaderLinker.ShaderLinker = function(program_obj) {
+	this.program_obj = program_obj;
+	this.initialize(program_obj);
+}
+
+//public:
+
+__ShaderLinker.initialize = function(program_obj) {
+	program_obj.link_status = false;
+
+	program_obj.attribute_locations = program_obj.default_attribute_locations;
+	program_obj.attributes = new Array(program_obj.max_attributes);
+
+	program_obj.uniform_locations = {};
+	program_obj.uniforms = new Array(program_obj.max_uniforms);
+
+	program_obj.varying_locations = {};
+	program_obj.varying = new Array(program_obj.max_varying);
+}
+
+
+__ShaderLinker.link = function(shader_objs, type) {
+
+	var oc, ex;
+
+	if (type == GL_VERTEX_SHADER) {
+		oc = 'vertex_object_code';
+		ex = 'vertex_executable';
+	} else {
+		oc = 'fragment_object_code';
+		ex = 'fragment_executable';
 	}
 
-	this.initialize = function(program_obj) {
-		program_obj.link_status = false;
+	this.program_obj[oc] = '';
 
-		program_obj.attribute_locations = program_obj.default_attribute_locations;
-		program_obj.attributes = new Array(program_obj.max_attributes);
+	for (var i in shader_objs) {
 
-		program_obj.uniform_locations = {};
-		program_obj.uniforms = new Array(program_obj.max_uniforms);
+		var shader = shader_objs[i];
 
-		program_obj.varying_locations = {};
-		program_obj.varying = new Array(program_obj.max_varying);
+		if (!this.addSymbols(shader.symbol_table)) {
+			return false;
+		}
+
+		this.program_obj[oc] += shader.object_code;				
+	}
+
+	this.program_obj[oc] += shader.object_code;
+	this.program_obj[ex] = eval(this.program_obj[oc]);
+
+	this.program_obj.link_status = true;
+	return true;
+}
+
+
+__ShaderLinker.addSymbols = function(symbols) {
+	for (var i = 0; i < symbols.length; i++) {
+		var symbol = symbols[i];
 		
-		program_obj.object_code = null;
-	}
-
-	this.addSymbols = function(symbols) {
-		for (var i = 0; i < symbols.length; i++) {
-			var symbol = symbols[i];
-			
-			if (symbol.type == 'attribute') {
-				var locations = this.program_obj.attribute_locations;
-				var list = this.program_obj.attributes;
-				var data = new cnvgl_vertex_attribute(symbol.name, symbol);
-			}
-			if (symbol.type == 'uniform') {
-				var locations = this.program_obj.uniform_locations;
-				var list = this.program_obj.uniforms;
-				var data = new cnvgl_uniform_variable(symbol.name, symbol);
-			}
-			if (symbol.type == 'varying') {
-				var locations = this.program_obj.varying_locations;			
-				var list = this.program_obj.varying;
-				var data = new cnvgl_varying_variable(symbol.name, symbol);
-			}
-
-			if (typeof locations[symbol.name] == 'undefined') {
-				var pos = this.findNewSymbolLocation(locations);
-				if (pos === false) {
-					return false;
-				}
-				locations[symbol.name] = pos;
-				list[pos] = data;
-			}
+		if (symbol.type == 'attribute') {
+			var locations = this.program_obj.attribute_locations;
+			var list = this.program_obj.attributes;
+			var data = new cnvgl_vertex_attribute(symbol.name, symbol);
 		}
-		return true;
-	}
-
-	this.findNewSymbolLocation = function(list) {
-		var found = [];
-		for (var i in list) {
-			found[list[i]] = i;
+		if (symbol.type == 'uniform') {
+			var locations = this.program_obj.uniform_locations;
+			var list = this.program_obj.uniforms;
+			var data = new cnvgl_uniform_variable(symbol.name, symbol);
 		}
-		//don't hardcode here
-		for (var i = 0; i < 32; i++) {
-			if (!found[i]) {
-				return i;	
-			}
-		}
-		return false;
-	}
-
-	this.link = function(shader_objs) {
-
-		if (!this.program_obj.vertex_object_code) {
-			this.program_obj.vertex_object_code = '';
-		}
-		if (!this.program_obj.fragment_object_code) {
-			this.program_obj.fragment_object_code = '';
+		if (symbol.type == 'varying') {
+			var locations = this.program_obj.varying_locations;			
+			var list = this.program_obj.varying;
+			var data = new cnvgl_varying_variable(symbol.name, symbol);
 		}
 
-		for (var i in shader_objs) {
-
-			var shader = shader_objs[i];
-			
-			if (!this.addSymbols(shader.symbol_table)) {
+		if (typeof locations[symbol.name] == 'undefined') {
+			var pos = this.findNewSymbolLocation(locations);
+			if (pos === false) {
 				return false;
 			}
-
-			if (shader.type == GL_VERTEX_SHADER) {
-				this.program_obj.vertex_object_code += shader.object_code;				
-			} else {
-				this.program_obj.fragment_object_code += shader.object_code;
-			}
+			locations[symbol.name] = pos;
+			list[pos] = data;
 		}
-
-		this.program_obj.link_status = true;
-		return true;
 	}
+	return true;
 }
-__ShaderLinker.prototype = new pClass;
-ShaderLinker.prototype = new __ShaderLinker;
+
+
+__ShaderLinker.findNewSymbolLocation = function(list) {
+	var found = [];
+	for (var i in list) {
+		found[list[i]] = i;
+	}
+	//don't hardcode here
+	for (var i = 0; i < 32; i++) {
+		if (!found[i]) {
+			return i;	
+		}
+	}
+	return false;
+}
+
 
