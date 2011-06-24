9b38558aed55c726c0a80c20179ab8909c60e99a
cshanahan.cimaron@gmail.com <cshanahan.cimaron@gmail.com@ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb>
2011-06-24 16:00:55 +0000

    Buffer unbinding should not empty old buffer
    git-svn-id: https://cwebgl.googlecode.com/svn/trunk@17 ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb

--- cnvGL/functions/glBindBuffer.js
+++ cnvGL/functions/glBindBuffer.js
@@ -9,21 +9,15 @@ function glBindBuffer(target, buffer) {
 	//@note: In the future, we probably should create the object here, not in glGenBuffers
 
 	//Initialize buffer object if necessary
+	/*
 	if (buffer != 0) {
 		var buffer_obj = cnvgl_objects[buffer];
 		buffer_obj.target = target;
 		buffer_obj.data = null;
 		buffer_obj.usage = GL_STATIC_DRAW;
 		//buffer_obj.access = GL_READ_WRITE;
-	}	
-
-	//Unbind old buffer
-	var old_buffer = cnvgl_state.bound_buffers[target];
-	if (old_buffer != 0) {
-		var old_buffer_object = cnvgl_objects[old_buffer];
-		old_buffer_object.target = null;
-		old_buffer_object.data = null;
 	}
+	*/
 
 	cnvgl_state.bound_buffers[target] = buffer;
 }
