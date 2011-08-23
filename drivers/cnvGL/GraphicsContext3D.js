5d9574621cca02ad644035ceb64d2da4a21981ec
cshanahan.cimaron <cshanahan.cimaron@ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb>
2011-08-23 15:33:02 +0000

    Adding automatic cnvgl internal resolution change
    git-svn-id: https://cwebgl.googlecode.com/svn/trunk@62 ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb

--- drivers/cnvGL/GraphicsContext3D.js
+++ drivers/cnvGL/GraphicsContext3D.js
@@ -23,38 +23,80 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 GraphicsContext3D = (function() {
 
 	function Initializer() {
-		//public:	
+		
+		//public:
+		this.buffer = null;
+		this.width = null;
+		this.height = null;
+		this.context = null;
+		
+		//private:
+		this._scale = null;
+		this._timer = null;
+		this._dirty = false;
+		this._tempWidth = null;
+		this._tempHeight = null;
+		this._tempCanvas = null;
+		this._tempCanvasCtx = null;
+		this._frameCount = null;
+		this._checkResChange = 0;
 	}
-	
+
 	var GraphicsContext3D = new jClass('GraphicsContext3D', Initializer);
 	
 	//public:
 	
 	GraphicsContext3D.GraphicsContext3D = function(canvas) {
 		this.context = canvas.getContext('2d');
-	
+
 		//create our cnvGL context here;
 		this.width = canvas.width;
 		this.height = canvas.height;
-	
-		var cnvgl_ctx = cnvgl_context.getCurrentContext();
-	
-		//initialize frame buffers
-		//cnvgl_state.color_buffer = new Uint8Array(this.width * this.height * 4);
-		this.buffer = this.context.createImageData(this.width, this.height);
-	
-		cnvgl_ctx.color_buffer = this.buffer.data;
-	
-		if (Float32Array.native) {
-			cnvgl_ctx.depth_buffer = new Float32Array(this.width * this.height);
-		} else {
-			cnvgl_ctx.depth_buffer = new Array(this.width * this.height);
-		}
-	
+		this._scale = 1;
+
+		this._createBuffer();
+
 		//initialize state
 		glClearColor(0, 0, 0, 255);
 		glClearDepth(1.0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		var This = this;
+		setInterval(function() { This._redraw(); }, 0);
+	};
+	
+	GraphicsContext3D._createBuffer = function() {
+		var cnvgl_ctx = cnvgl_context.getCurrentContext();
+		var width, height;
+
+		if (this._scale > 1) {
+			this._tempCanvas = document.createElement('canvas');
+			this._tempWidth = Math.round(this.width / this._scale);
+			this._tempHeight = Math.round(this.height / this._scale);
+			this._tempCanvas.width = this._tempWidth;
+			this._tempCanvas.height = this._tempHeight;
+			this._tempCanvasCtx = this._tempCanvas.getContext('2d');
+			this._tempCanvasCtx.mozImageSmoothingEnabled = false;
+			
+			width = this._tempWidth;
+			height = this._tempHeight;
+			
+		} else {
+			width = this.width;
+			height = this.height;
+		}
+		
+		//initialize frame buffers
+		//cnvgl_state.color_buffer = new Uint8Array(this.width * this.height * 4);
+		this.buffer = this.context.createImageData(width, height);
+
+		cnvgl_ctx.color_buffer = this.buffer.data;
+
+		if (Float32Array.native) {
+			cnvgl_ctx.depth_buffer = new Float32Array(width * height);
+		} else {
+			cnvgl_ctx.depth_buffer = new Array(width * height);
+		}		
 	};
 	
 	//public:
@@ -77,9 +119,9 @@ GraphicsContext3D = (function() {
 	
 	GraphicsContext3D.clear = function(mask) {
 		glClear(mask);
-		this.setRedraw();
+		this._dirty = true;
 	};
-	
+
 	GraphicsContext3D.clearColor = function(red, green, blue, alpha) {
 		glClearColor(red, green, blue, alpha);
 	};
@@ -104,9 +146,9 @@ GraphicsContext3D = (function() {
 	
 	GraphicsContext3D.drawArrays = function(mode, first, count) {
 		glDrawArrays(mode, first, count);
-		this.setRedraw();
+		this._dirty = true;
 	};
-	
+
 	GraphicsContext3D.enable = function(cap) {
 		glEnable(cap);
 	};
@@ -161,22 +203,52 @@ GraphicsContext3D = (function() {
 		var pointer = [];
 		glVertexAttribPointer(idx, size, type, normalized, stride, offset);
 	};
-	
+
 	GraphicsContext3D.viewport = function(x, y, width, height) {
-		glViewport(x, y, width, height);
+		glViewport(x / this._scale, y / this._scale, width / this._scale, height / this._scale);
 	};
-	
+
 	//private:
-	GraphicsContext3D.setRedraw = function() {
-		this.redrawing = true;
-		var This = this;
-		setTimeout(function() { This.redraw(); }, 0);
-	};
-	
-	GraphicsContext3D.redraw = function() {
-		if (this.redrawing) {
-			this.context.putImageData(this.buffer, 0, 0);
-			this.redrawing = false;
+	GraphicsContext3D._redraw = function() {
+		if (this._dirty) {
+			this._frameCount++;
+			this._dirty = false;
+
+			if (this._scale == 1) {
+				this.context.putImageData(this.buffer, 0, 0);
+			} else {
+				this._tempCanvasCtx.putImageData(this.buffer, 0, 0);
+				this.context.drawImage(this._tempCanvas, 0, 0, this.width, this.height);
+			}
+
+			//automatically adjust resolution for next frame
+			var time, dur;
+			time = new Date().getTime();
+			if (this._time) {
+				dur = time - this._time;
+			}
+			this._time = time;
+
+			if (dur > 500 && this._scale < 8) {
+				this._checkResChange--;
+			} else {
+				if (dur < 50 && this._scale > 1) {
+					this._checkResChange++;
+				} else {
+					this._checkResChange = 0;
+				}
+			}
+
+			if (this._checkResChange < -2) {
+				this._scale *= 2;
+				this._createBuffer();				
+			}
+
+			if (this._checkResChange > 10) {
+				this._scale /= 2;
+				this._createBuffer();
+				this._checkResChange = 0;
+			}
 		}
 	};
 	
