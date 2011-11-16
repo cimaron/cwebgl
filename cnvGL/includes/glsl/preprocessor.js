10138ab53eca8c57c613f5e69b71b45b05433c4a
cwebgl@cimaron.net <cwebgl@cimaron.net@ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb>
2011-11-16 16:37:08 +0000

    Adding comment stripping to preprocessor
    git-svn-id: https://cwebgl.googlecode.com/svn/trunk@168 ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb

--- cnvGL/includes/glsl/preprocessor.js
+++ cnvGL/includes/glsl/preprocessor.js
@@ -31,13 +31,29 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 		errors : [],
 
 		preprocess : function(source) {
+			var s, e;
 			
 			//reinitialize
 			this.status = false;
 			this.errors = [];
 			
 			//pretty basic, just want to make it work for right now
+			
+			//remove preprocessor directives
 			this.output = source.replace(/[ \t]*\#[^\n]+/g, '');
+
+			//remove single-line comments
+			this.output = this.output.replace(/\/\/[^\n]*/g, '');
+
+			//remove multi-line comments
+			while ((s = this.output.indexOf("/*")) != -1) {
+				if ((e = this.output.indexOf("*/", s + 2)) == -1) {
+					this.status = false;
+					this.errors.push("Unterminated comment");
+					return false;
+				}
+				this.output = this.output.slice(0, s) + this.output.slice(e + 2);
+			}
 
 			this.status = true;
 			return true;
