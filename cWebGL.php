4a9b195715fb603e4079fa3269cd6d37444d1650
cwebgl@cimaron.net <cwebgl@cimaron.net@ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb>
2012-01-05 22:04:29 +0000

    Fixing include path problems in some environments
    git-svn-id: https://cwebgl.googlecode.com/svn/trunk@244 ef9c5acb-30c7-f37c-82dd-5f4c3dad93eb

--- cWebGL.php
+++ cWebGL.php
@@ -20,7 +20,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-require_once 'include.php';
+require_once dirname(__FILE__).'/include.php';
 
 //Extra Libraries
 $include('library/jClass/jClass.js');
